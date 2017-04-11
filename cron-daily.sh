bd68676b70a1b22746763bcfb89cc6a15164ca7f
cquest <cquest@openstreetmap.fr>
2017-04-11 17:36:40 +0200

    tri par SIRET et N° inspection

--- cron-daily.sh
+++ cron-daily.sh
@@ -5,7 +5,7 @@ d=`date +%Y-%m-%d`
 curl -s 'https://dgal.opendatasoft.com/explore/dataset/export_alimconfiance/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true' > export_alimconfiance_$d.csv
 
 # nettoyage ; > , + suppression date extraction + tri par SIRET
-csvcut -d ';' -C Date_extraction export_alimconfiance_$d.csv | csvsort -c SIRET > export_alimconfiance.csv
+csvcut -d ';' -C Date_extraction export_alimconfiance_$d.csv | csvsort -c SIRET,Numero_inspection > export_alimconfiance.csv
 
 # commit git + push sur github
 git commit -a -m "$d"
