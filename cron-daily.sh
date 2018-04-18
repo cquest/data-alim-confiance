8dc39be4e102bba016edd8b6266b29ccad296079
cquest <cquest@openstreetmap.fr>
2018-04-18 08:00:44 +0200

    2018-04-18

--- cron-daily.sh
+++ cron-daily.sh
@@ -2,7 +2,7 @@
 d=`date +%Y-%m-%d`
 
 # récupération fichier du jour
-curl -s 'https://dgal.opendatasoft.com/explore/dataset/export_alimconfiance/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true' > export_alimconfiance_$d.csv
+curl -sL 'https://dgal.opendatasoft.com/explore/dataset/export_alimconfiance/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true' > export_alimconfiance_$d.csv
 
 # nettoyage ; > , + remise en ordre des colonnes + tri par SIRET
 csvcut -d ';' -c APP_Libelle_activite_etablissement,APP_Libelle_etablissement,ods_adresse,Code_postal,Libelle_commune,Libelle_commune,Date_inspection,Synthese_eval_sanit,Agrement,geores,ods_adresse,filtre,Numero_inspection,SIRET export_alimconfiance_$d.csv | csvsort -c SIRET,Numero_inspection > export_alimconfiance.csv
