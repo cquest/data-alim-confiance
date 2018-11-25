8a9d2506b1e95778de9cca84a0b6044b9fb1c588
cquest <cquest@openstreetmap.fr>
2018-11-25 20:23:29 +0100

    2018-11-25

--- cron-daily.sh
+++ cron-daily.sh
@@ -7,22 +7,23 @@ curl -sL 'https://dgal.opendatasoft.com/explore/dataset/export_alimconfiance/dow
 # nettoyage ; > , + remise en ordre des colonnes + tri par SIRET
 csvcut -d ';' -c APP_Libelle_activite_etablissement,APP_Libelle_etablissement,ods_adresse,Code_postal,Libelle_commune,Libelle_commune,Date_inspection,Synthese_eval_sanit,Agrement,geores,ods_adresse,filtre,Numero_inspection,SIRET export_alimconfiance_$d.csv | csvsort -c SIRET,Numero_inspection > export_alimconfiance.csv
 
-# ajout au fichier compressé d'archive
-7z u exports_alim_confiance.7z export_alimconfiance_$d.csv
-
 # commit git + push sur github
 git commit -a -m "$d"
-
 
 . ~/.keychain/cquest-Precision-WorkStation-T7500-sh
 
 git push
+
+# on range le fichier pour archive...
+mv export_alimconfiance_$d.csv exports/
+
+cd exports
+# ajout au fichier compressé d'archive
+7z u ../exports_alim_confiance.7z *.csv
+cd -
 
 # envoi sur scaleway
 rsync exports* root@data.cquest.org:/var/www/html/data/alim_confiance/ -az
 
 # envoi vers OpenEventDatabase des nouveaux contrôles
 ~/.virtualenvs/oedb/bin/python dgal2oedb.py export_alimconfiance_$d.csv
-
-# on range le fichier pour archive...
-mv export_alimconfiance_$d.csv exports/
