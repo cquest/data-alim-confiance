3a39486f9ba5ef7652dee2b69ec2ba3eff42a946
cquest <cquest@openstreetmap.fr>
2021-06-13 19:00:31 +0200

    2021-06-13

--- cron-daily.sh
+++ cron-daily.sh
@@ -6,7 +6,7 @@ YEAR=$(date +%Y)
 curl -sL 'https://dgal.opendatasoft.com/explore/dataset/export_alimconfiance/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true' > export_alimconfiance_$d.csv
 
 # nettoyage ; > , + remise en ordre des colonnes + tri par SIRET
-csvcut -d ';' -c APP_Libelle_activite_etablissement,APP_Libelle_etablissement,ods_adresse,Code_postal,Libelle_commune,Libelle_commune,Date_inspection,Synthese_eval_sanit,Agrement,geores,ods_adresse,filtre,Numero_inspection,SIRET export_alimconfiance_$d.csv | csvsort -c SIRET,Numero_inspection > export_alimconfiance.csv
+csvcut -d ';' -c APP_Libelle_activite_etablissement,APP_Libelle_etablissement,Adresse_2_UA,Code_postal,Libelle_commune,Libelle_commune,Date_inspection,Synthese_eval_sanit,Agrement,geores,Adresse_2_UA,filtre,Numero_inspection,SIRET export_alimconfiance_$d.csv | csvsort -c SIRET,Numero_inspection > export_alimconfiance.csv
 
 # commit git + push sur github
 git commit -a -m "$d"
